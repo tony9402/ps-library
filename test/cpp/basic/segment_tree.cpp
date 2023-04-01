7e0309cba08f2febab9543b24c85c528ed7748af
tony9402 <tony9402@naver.com>
2023-04-01 16:02:07 +0300

    [ADD] test code for segment tree

---
+++ test/cpp/basic/segment_tree.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+[[segment_tree]]
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N, M, K; cin >> N >> M >> K;
+    Segment<long long> tree(N);
+    for(int i = 0; i < N; ++i) {
+        long long x; cin >> x;
+        tree.update(i, x);
+    }
+    for(int i = 0; i < M + K; ++i) {
+        long long cmd, a, b; cin >> cmd >> a >> b;
+        if(cmd == 1) tree.update(a - 1, b);
+        else cout << tree.query(a - 1, b - 1) << '\n';
+    }
+    return 0;
+}
