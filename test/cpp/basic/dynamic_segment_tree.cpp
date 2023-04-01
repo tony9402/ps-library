f78d03b05f7747a5ecd95e25206073f9e47fd2e6
tony9402 <tony9402@naver.com>
2023-04-01 09:02:07 +0300

    [ADD] add test code for dynamic segment tree

---
+++ test/cpp/basic/dynamic_segment_tree.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+[[dynamic_segment_tree]]
+DynamicSegment<long long> tree;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N, M, K; cin >> N >> M >> K;
+    for(int i = 1; i <= N; ++i) {
+        long long x; cin >> x;
+        tree.update(i, x);
+    }
+    for(int i = 0; i < M + K; ++i) {
+        long long a, b, c; cin >> a >> b >> c;
+        if(a == 1) tree.update(b, c);
+        else cout << tree.query(b, c) << '\n';
+    }
+    return 0;
+}
