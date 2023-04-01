ed8f5867f9bfc010a810268c106edf6c68f4cbc1
tony9402 <tony9402@naver.com>
2023-04-01 16:44:43 +0300

    [MOD] tested

--- src/cpp/basic/power.cpp
+++ src/cpp/basic/power.cpp
@@ -1,7 +1,7 @@
 /*
- * Problem for Testing Template : 
- * Test : 
- * Last Updated : 
+ * Problem for Testing Template : https://www.acmicpc.net/problem/1629
+ * Test : Accepted
+ * Last Updated : 2023.04.01 10:44 PM (KST)
  */
 template<typename T, typename modulo>
 T power(T a, T b, modulo mod) {
