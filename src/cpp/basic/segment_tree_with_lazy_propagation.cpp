b2cb5d34e05a5221055581f4eb8b22f8994d4b85
tony9402 <tony9402@naver.com>
2023-04-01 15:57:04 +0300

    [FIX] Fixed SegmentLazy Template

--- src/cpp/basic/segment_tree_with_lazy_propagation.cpp
+++ src/cpp/basic/segment_tree_with_lazy_propagation.cpp
@@ -1,7 +1,7 @@
 /*
- * Problem for Testing Template : 
- * Test : 
- * Last Updated : 
+ * Problem for Testing Template : https://www.acmicpc.net/problem/10999
+ * Test : Accepted
+ * Last Updated : 2023.04.01 09:56 PM (KST)
  */
 template<typename T>
 struct SegmentLazy {
@@ -12,11 +12,15 @@ struct SegmentLazy {
         for(siz = 1; siz < N; siz <<= 1);
         lazy = tree = vector<T>(siz << 1);
     }
+    void putItem(int idx, T data) { tree[idx + siz] = data; }
+    void build() {
+        for(int i = siz - 1; i; --i) tree[i] = merge(tree[i << 1], tree[i << 1 | 1]);
+    }
     void propagate(int l, int r, int pos) {
         if(!lazy[pos]) return;
         if(l != r) {
-            lazy[pos << 1] += lazy[pos];
-            lazy[pos << 1 | 1] += lazy[pos];
+            lazy[pos << 1] = merge(lazy[pos << 1], lazy[pos]);
+            lazy[pos << 1 | 1] = merge(lazy[pos << 1 | 1], lazy[pos]);
         }
         tree[pos] += lazy[pos] * (r - l + 1);
         lazy[pos] = 0;
@@ -27,6 +31,7 @@ struct SegmentLazy {
             propagate(l, r, pos);
             return;
         }
+        propagate(l, r, pos);
         if(e <  l || r <  s) return;
         int mid = (l + r) / 2;
         update(l, mid, s, e, pos << 1, data);
@@ -42,4 +47,5 @@ struct SegmentLazy {
         return merge(query(l, mid, s, e, pos << 1), query(mid + 1, r, s, e, pos << 1 | 1));
     }
     T query(int s, int e) { return query(0, siz - 1, s, e, 1); }
+    T merge(T a, T b) { return a + b; }
 };
