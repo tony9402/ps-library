d9e10687a0fbfabcfda800a24c04149b961bbc62
tony9402 <tony9402@naver.com>
2023-04-01 16:44:55 +0300

    [ADD] test code for power template

---
+++ test/cpp/basic/power.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+[[power]]
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    long long A, B, C; cin >> A >> B >> C;
+    cout << power(A, B, C);
+
+    return 0;
+}
