4afa3d1ccc5725ad45867af37bb484f5dfab1fdc
tony9402 <tony9402@naver.com>
2023-11-28 20:24:33 +0900

    BFS, tcmain, main 추가

---
+++ src/cpp/basic/bfs.cpp
@@ -0,0 +1,20 @@
+[[graph]]
+
+struct BFS {
+    Graph<int> G;
+    BFS(const Graph<int> _G):G(_G) { }
+    vector<int> operator()(int S) {
+        vector<int> D(G.size(), -1);
+        queue<int> Q({S});
+        D[S] = 0;
+        while(!Q.empty()) {
+            int cur = Q.front(); Q.pop();
+            for(int nxt: G[cur]) {
+                if(D[nxt] != -1) continue;
+                D[nxt] = D[cur] + 1;
+                Q.emplace(nxt);
+            }
+        }
+        return D;
+    }
+};
---
+++ src/cpp/basic/main.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef bool i1;
+typedef char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
+
+typedef float f4;
+typedef double f8;
+typedef long double f16;
+
+template<typename T> using Vec = vector<T>;
+
+template<int fp=0> struct fastio { fastio() { ios::sync_with_stdio(false); cin.tie(0); if(fp)cout<<fixed<<' '<<setprecision(fp); } };
+
+template<typename First, typename Second> inline istream& operator>>(istream &in, pair<First, Second> &_data) { in>>_data.first>>_data.second; return in; }
+template<typename First, typename Second> inline ostream& operator<<(ostream &out, pair<First, Second> &_data) { out<<_data.first<<' '<<_data.second; return out; }
+template<typename First, typename Second, typename Third> inline istream& operator>>(istream &in, tuple<First, Second, Third> &_data) { in>>get<0>(_data)>>get<1>(_data)>>get<2>(_data); return in; }
+template<typename First, typename Second, typename Third> inline ostream& operator<<(ostream &out, tuple<First, Second, Third> &_data) { out<<get<0>(_data)<<' '<<get<1>(_data)<<' '<<get<2>(_data); return out; }
+
+template<typename InputType> void in(InputType& x) { cin>>x; }
+template<typename InputType, typename... InputTypes> void in(InputType& x, InputTypes& ...y) { cin>>x; in(y...); }
+template<typename IterableInputType> void vin(IterableInputType &V, int skip=0) { for(auto &x: V) if(--skip < 0) cin >> x; }
+
+template<const int p=0, typename OutputType> void out(OutputType x) { cout<<x<<' '; }
+template<const int p=0, typename OutputType, typename... OutputTypes> void out(OutputType x, OutputTypes ...y) { cout<<fixed<<setprecision(p)<<x<<' '; out<p>(y...); }
+template<const int p=0, typename IterableOutputType> void vout(const IterableOutputType &V, int skip=0) { for(auto &x: V) if(--skip<0) out<p>(x); }
+
+template<i64 modulo=numeric_limits<i64>::max(), typename... T> i64 Sum(T... x) { return (... + x) % modulo; }
+template<i64 modulo=numeric_limits<i64>::max(), typename... T> i64 Mul(T... x) { return (... * x) % modulo; }
+
+constexpr int dy[] = {-1,1,0,0,-1,-1,1,1,-2,-1,1,2,2,1,-1,-2};
+constexpr int dx[] = {0,0,-1,1,-1,1,-1,1,1,2,2,1,-1,-2,-2,-1};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ${1}
+}
