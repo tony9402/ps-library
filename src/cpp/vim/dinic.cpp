2a7bff5dce13c11fe078ca4878961af910ce3f13
tony9402 <tony9402@naver.com>
2024-05-21 12:32:37 +0900

    dinic 포인터 없는 버전으로 업데이트

--- src/cpp/vim/dinic.cpp
+++ src/cpp/vim/dinic.cpp
@@ -1,115 +1,104 @@
 /*
  * Problem for Testing Template : https://www.acmicpc.net/problem/11377
  * Test : Accepted
- * Last Updated : 2023.04.01 12:52 AM (KST)
+ * Last Updated : 2024.05.21 12:29 AM (KST)
  * Tag : dinic, flow
  */
 struct Dinic {
-    struct Edge {
-        int to, c, f;
-        Edge* r;
-        Edge() { Edge(-1, 0); }
-        Edge(int _to, int _c) :to(_to), c(_c), f(0), r(nullptr) { }
-        int spare() { return c - f; }
-        void setRev(Edge* rev) { r = rev; }
+    struct Node {
+        int node_idx, cost, flow, rev;
+        Node(int _nxt = -1, int _cost = 0, int _rev = -1):node_idx(_nxt),cost(_cost),flow(0),rev(_rev) { }
+        int spare() { return cost - flow; }
+        void setRev(int _rev) { rev = _rev; }
     };
+    vector<Node> nodes;
+    vector<vector<int>> G;
+    vector<int> level;
+    vector<int> work;
 
-    vector<vector<Edge*>> graph;
-    vector<int> level, work;
-    int src, snk, asrc, asnk;
-
-    Dinic() { }
-    void init(int N) {
-        src = N + 1; snk = src + 1; asrc = snk + 1; asnk = asrc + 1;
-        N += 5;
-        graph.resize(N); level.resize(N); work.resize(N);
+    int src, snk, asrc, asnk, N;
+    Dinic(int _N) {
+        src = _N + 1;
+        snk = src + 1;
+        asrc = snk + 1;
+        asnk = asrc + 1;
+        N = asnk;
+        G.resize(N + 1);
     }
-    void clear() { graph.clear(); level.clear(); work.clear(); }
-
     bool bfs(int s, int e) {
-        fill(level.begin(), level.end(), -1);
+        level = vector<int>(N + 1, -1);
         level[s] = 0;
-
-        queue<int> q;
-        q.push(s);
-
-        while (!q.empty()) {
-            int t = q.front(); q.pop();
-
-            for (auto i : graph[t]) {
-                if (i->spare() > 0 && level[i->to] == -1) {
-                    level[i->to] = level[t] + 1;
-                    q.push(i->to);
+        queue<int> Q; Q.push(s);
+        while(!Q.empty()) {
+            int cur = Q.front(); Q.pop();
+            for(const int &x: G[cur]) {
+                Node &nxt = nodes[x];
+                if(nxt.spare() > 0 && level[nxt.node_idx] == -1) {
+                    level[nxt.node_idx] = level[cur] + 1;
+                    Q.push(nxt.node_idx);
                 }
             }
         }
-        return level[e] != -1;
+        return ~level[e];
     }
-
     int dfs(int s, int e, int f) {
-        if (s == e)return f;
-
-        for (int& i = work[s]; i < (int)graph[s].size(); i++) {
-            Edge* now = graph[s][i];
-            if (level[now->to] == level[s] + 1 && now->spare() > 0) {
-                int ret = dfs(now->to, e, min(f, now->spare()));
-                if (ret > 0) {
-                    now->f += ret;
-                    now->r->f -= ret;
+        if(s == e) return f;
+        for(int &i = work[s]; i < (int)G[s].size(); ++i) {
+            Node &nxt = nodes[G[s][i]];
+            if(nxt.spare() > 0 && level[nxt.node_idx] == level[s] + 1) {
+                int ret = dfs(nxt.node_idx, e, min(f, nxt.spare()));
+                if(ret > 0) {
+                    nxt.flow += ret;
+                    nodes[nxt.rev].flow -= ret;
                     return ret;
                 }
             }
         }
         return 0;
     }
-
-    int Flow(int s, int e) {
-        int flow = 0;
-        while (bfs(s, e)) {
-            fill(work.begin(), work.end(), 0);
-            while (1) {
-                int ret = dfs(s, e, 0x3f3f3f3f);
-                if (ret == 0)break;
-                flow += ret;
+    int flow(int s, int e) {
+        int ret = 0;
+        while(bfs(s, e)) {
+            work = vector<int>(N + 1, 0);
+            while(true) {
+                int x = dfs(s, e, numeric_limits<int>::max());
+                if(x == 0) break;
+                ret += x;
             }
         }
-        return flow;
+        return ret;
     }
-
-    Edge* addEdge(int u, int v, int c, bool directed = true, bool Unique = false) {
-        if (Unique) {
-            for (auto i : graph[u]) {
-                if (i->to == v) {
-                    i->c += c;
-                    if (!directed)return i;
+    void addEdge(int u, int v, int cost, bool is_directed = true, bool is_unique = false) {
+        if(is_unique) {
+            for(const int &x: G[u]) {
+                if(nodes[x].node_idx == v) {
+                    nodes[x].cost += cost;
+                    if(!is_directed) return;
                     break;
                 }
             }
-            if (!directed) {
-                for (auto i : graph[v]) {
-                    if (i->to == u) {
-                        i->c += c;
-                        return i;
+            if(!is_directed) {
+                for(const int &x: G[v]) {
+                    if(nodes[x].node_idx == u) {
+                        nodes[x].cost += cost;
+                        return;
                     }
                 }
             }
         }
-
-        Edge* uv = new Edge(v, c);
-        Edge* vu = new Edge(u, directed ? 0 : c);
-        uv->setRev(vu); vu->setRev(uv);
-        graph[u].push_back(uv);
-        graph[v].push_back(vu);
-
-        return uv;
+        int a = (int)nodes.size(), b = a + 1;
+        Node uv = Node(v, cost, b);
+        Node vu = Node(u, is_directed ? 0 : cost, a);
+        nodes.push_back(uv); nodes.push_back(vu);
+        G[u].push_back(a); G[v].push_back(b);
     }
-
-    Edge* addLREdge(int u, int v, int lower, int upper) {
-        if (lower) {
+    void addLREdge(int u, int v, int lower, int upper) {
+        if(lower) {
             addEdge(asrc, v, lower);
             addEdge(u, asnk, lower);
         }
-
-        return addEdge(u, v, upper - lower);
+        addEdge(u, v, upper - lower);
     }
+    int flow() { return flow(src, snk); }
+    int lrflow() { return flow(asrc, asnk); }
 };
--- test/cpp/basic/dinic.cpp
+++ test/cpp/basic/dinic.cpp
@@ -3,13 +3,12 @@
 using namespace std;
 
 [[dinic]]
-Dinic flow;
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    flow.init(2000);
+    Dinic flow(2000);
     int N, M, K; cin >> N >> M >> K;
     flow.addEdge(flow.src, flow.asrc, K);
     for(int i = 0; i < N; ++i) {
@@ -22,7 +21,7 @@ int main(){
         flow.addEdge(flow.asrc, i, 1);
     }
     for(int i = 0; i < M; ++i) flow.addEdge(i + 1000, flow.snk, 1);
-    cout << flow.Flow(flow.src, flow.snk);
+    cout << flow.flow();
 
     return 0;
 }
